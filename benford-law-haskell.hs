ca84abebc630d740bb5a29ff8100a89bc2cc830e
Kauan Takeda <kauan.takeda@creditas.com.br>
2020-10-18 14:45:58 -0300

    [ add ] benford law

---
+++ benford-law-haskell.hs
@@ -0,0 +1,40 @@
+--Kauan Takeda - 156104
+
+import Data.List
+
+isNumberOrDot :: Char -> Bool
+isNumberOrDot x = x `elem` "1234567890."
+
+isLastNumber :: Char -> Char -> Bool
+isLastNumber actualChar nextChar = if (isNumberOrDot actualChar) && (not (isNumberOrDot nextChar))
+                                          then True
+                                        else
+                                          False
+
+getLastNumbers :: String -> Char -> String
+getLastNumbers [] nextChar = ""
+getLastNumbers [x] nextChar = if isNumberOrDot x
+                              then [x]
+                            else
+                              ""
+getLastNumbers (x:xs) nextChar = if isLastNumber x nextChar
+                                      then x : getLastNumbers xs (xs !! 1)
+                                    else
+                                      getLastNumbers xs (xs !! 1)
+
+getFirstNumbers :: String -> String
+getFirstNumbers [] = ""
+getFirstNumbers [x] = if isNumberOrDot x
+                        then [x]
+                      else
+                        ""
+getFirstNumbers text = reverse $ getLastNumbers (reverse text) ((reverse text) !! 1)
+
+groupStringByEqualsElements :: String -> [String]
+groupStringByEqualsElements [] = []
+groupStringByEqualsElements x = group $ sort x
+
+countOccurrence :: String -> [(String, Int)]
+countOccurrence [] = [("", 0)]
+countOccurrence text = map makeOccurrenceTuples $ groupStringByEqualsElements $ getFirstNumbers text
+  where makeOccurrenceTuples (x:xs) = ([x], length (x:xs))
