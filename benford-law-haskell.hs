2a8301fcc8fd41a0a273e20e41f57992a1c6fdf9
kauantakeda <33906831+kauantakeda@users.noreply.github.com>
2020-10-28 18:25:59 -0300

    [add] main, computeTotal, computeFrequency, tupleTostring

--- benford-law-haskell.hs
+++ benford-law-haskell.hs
@@ -2,7 +2,14 @@
 --Waki
 --Camila
 
+
 import Data.List
+
+main = do
+       contents <- getContents
+       let occurrence = countOccurrence contents
+       let frequencies = computeFrequency occurrence (computeTotal occurrence)
+       putStr (tupleToString frequencies)
 
 isNumberOrDot :: Char -> Bool
 isNumberOrDot x = x `elem` "1234567890."
@@ -27,9 +34,25 @@ getFirstNumbers (x:xs) alreadyGot = if isNumberOrDot x
 
 groupStringByEqualsElements :: String -> [String]
 groupStringByEqualsElements [] = []
-groupStringByEqualsElements x = group $ sort x
+groupStringByEqualsElements x = group $ sort (x ++ "123456789")
+
+makeOccurrenceTuplesList :: String -> [(String, Int)]
+makeOccurrenceTuplesList [] = []
+makeOccurrenceTuplesList firstNumbers = map makeOccurrenceTuple $ groupStringByEqualsElements firstNumbers
+  where makeOccurrenceTuple (x:xs) = ([x], length (x:xs) - 1)
 
 countOccurrence :: String -> [(String, Int)]
 countOccurrence [] = [("", 0)]
-countOccurrence text = map makeOccurrenceTuples $ groupStringByEqualsElements $ getFirstNumbers text False
-  where makeOccurrenceTuples (x:xs) = ([x], length (x:xs))
+countOccurrence text = makeOccurrenceTuplesList $ getFirstNumbers text False
+  
+computeTotal :: [(String, Int)] -> Int
+computeTotal [] = 0
+computeTotal (x:xs) = snd x + computeTotal xs
+
+computeFrequency :: [(String, Int)] -> Int -> [(String, Float)]
+computeFrequency [] total = []
+computeFrequency (x:xs) total = (fst x, fromIntegral (snd x) / fromIntegral total) : computeFrequency xs total
+
+tupleToString :: [(String, Float)] -> String
+tupleToString [] = ""
+tupleToString (x:xs) = fst x ++ " : " ++ show ( snd x ) ++ "\n" ++ tupleToString xs
